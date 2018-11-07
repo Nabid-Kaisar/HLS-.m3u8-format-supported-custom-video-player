2e238fbc154fc8edbc22b2299d1d4b99ee0c1715
Nabid-Kaisar <himelkaisar023@gmail.com>
2018-11-07 17:19:53 +0600

    customized for live stream and added m3u8 functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Player } from 'video-react';
+import HLSSource from './HLSSource';
+import "../node_modules/video-react/dist/video-react.css";
+import "./App.css"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default (props) => {
+  // Add customized HLSSource component into video-react Player
+  // The Component with `isVideoChild` attribute will be added into `Video` component
+  // Please use this url if you test it from local:
+  // http://www.streambox.fr/playlists/x36xhzz/x36xhzz.m3u8
+  return (
+    <Player>
+      <HLSSource
+        isVideoChild
+        src= "http://www.streambox.fr/playlists/test_001/stream.m3u8"
+      />
+    </Player>
+  );
+};
